7d18e1455de06d023237ce9e26feaf3c85877560
Rhenaud Dubois <rhdubois@emi.u-bordeaux1.fr>
2017-12-02 15:44:54 +0100

    Update server + pong_lib

--- client/pong_lib.py
+++ client/pong_lib.py
@@ -21,3 +21,22 @@ def load(ball,ball_coords,rackets,rackets_coord):
     rackets = [None,None]
     rackets_coords = [None,None]
 
+class Ball:
+	def __init__(self):
+		self.x = width / 2
+		self.y = random.randrange(height / 4,height * 3 / 4)
+		self.direction = (random.randrange(-60, 60) + 360) % 360
+		if random.randrange(2) > 0: 
+			self.direction = (180 - self.direction + 360) % 360
+		self.direction = radians(self.direction)
+		self.speed = 4
+		self.width = 20
+		self.height = 20
+
+	def update():
+		self.x = self.x + speed * math.cos(self.direction)
+		self.y = self.y + speed * math.sin(self.direction)
+		if self.x < self.width / 2 or self.x >= width - self.width / 2:
+			self.direction = -self.direction
+		if self.y < self.height / 2 or self.y >= height - self.height / 2:
+			math.pi - self.direction
--- server/server.py
+++ server/server.py
@@ -2,35 +2,78 @@
 import socket
 import select
 import threading
+import sys
+import pygame
+import random
+import math
+
 server = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(('', 10000))
 server.listen(1)
 players = [] #Max 2
 
+started = False
+ball = []
+width = 800 #temp
+height = 600 #temp
+
+#Main function of the program
 def main():
-	while True:
-		read, w, ex = select.select([server]+players,[],[]) #Get all sockets with action
-		for s in read:
-			if s == server: #New connection
-				print("Connexion received")
-				if len(players) >= 2: #Max 2 players
-					print("Max connection number reached")
-					s2, tmp = s.accept()
-					s2.send("Max connection number reached")
-					s2.close()
-					continue
+	if len(players) < 2:
+		acceptPlayers()
+	else
+		computeGame()
+
+#Accept players when under 2 players
+def acceptPlayers():
+	read, w, ex = select.select([server]+players,[],[]) #Get all sockets with action
+	for s in read:
+		if s == server: #New connection
+			print("Connexion received")
+			if len(players) >= 2: #Max 2 players
+				print("Max connection number reached")
 				s2, tmp = s.accept()
-				s2.send("Welcome to the Pong server! (NB: This is a ping server for now)")
-				players.append(s2)
+				s2.send("Max connection number reached")
+				s2.close()
+				continue
+			s2, tmp = s.accept()
+			s2.send("Welcome to the Pong server! Waiting for another player...")
+			players.append(s2)
+		else:
+			data = s.recv(4096)
+			if not data: 
+				print("Connexion closed")
+				s.send(data)
+				s.close()
+				players.remove(s)
 			else:
-				data = s.recv(4096)
-				if not data: 
-					print("Connexion closed")
-					s.send(data)
-					s.close()
-					players.remove(s)
-				else:
-					s.send(data)
+				s.send(data)
+
+#Disconnect player if the connection has been closed
+def detectDisconnect(player):
+	player.close()
+	players.remove(player)
+
+def startGame():
+	global width
+	global height
+	for s in players:
+		s.send("Start")
+	throwBall()
+
+def computeGame():
+	if not started:
+		started = True
+		startGame()
+
+def computeReceivedData(data):
+	print("I live!")
+
+def sendGameData():
+	print("I live!")
+
+def throwBall():
+	ball = Ball()
 
 main()
