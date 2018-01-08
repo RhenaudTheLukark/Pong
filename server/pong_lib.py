b0c7b255b98a63ec8645a7b52461230382968f2d
RhenaudTheLukark <rhen33390@gmail.com>
2018-01-08 12:14:15 +0100

    Add effects server side

--- server/pong_lib.py
+++ server/pong_lib.py
@@ -34,7 +34,7 @@ class ItemServer:
 		self.y = 0 #Temporary
 		self.speed = 1
 		self.dim = 40
-	        self.type = random.randrange(1, 4)
+	        self.type = random.randrange(1, 5)
 	        self.id = n
 		self.y = random.randrange(0, height - self.dim)
             
